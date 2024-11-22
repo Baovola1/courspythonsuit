765f36734553b3dd89232e0b6e1a68d33268a041
Baovola1 <baovolarav@gmail.com>
2024-11-22 21:38:49 +0100

    autre syntaxe utilisation zip() et boucle for

--- Cours/for.py
+++ Cours/for.py
@@ -1,23 +1,11 @@
 #Utilisation de la boucle FOR(Sert à parcourir une collection ou examiner 1 à 1 les éléments)
-iterable = ["Hello","Bonjour","Welcome"]
-
-iterator = iter(iterable)#Ici, on itère la liste
-
-objet = next(iterator)#next(iterator)=> permet de chercher l'élément suivant
-print(objet)
-
-objet = next(iterator)
-print(objet)
-
-objet = next(iterator)
-print(objet)
 
 #Les bonnes pratiques pour travailler avec la boucle FOR
 factures = ['Fac.1','Fac.2','Fac.3','Fac.4']
 
 for each in factures:
     print(each)
-#utilisation itérator
+#utilisation itérator=> on peut parcourir qu'une seule collection
 factures = ['Fac.1','Fac.2','Fac.3','Fac.4']
  
 for compteur, facture in enumerate(factures):
@@ -26,3 +14,15 @@ for compteur, facture in enumerate(factures):
 #autre syntaxe
 for i,facture in enumerate(factures):
     print(i,facture)
+#UTILISATION de zip()=> sert à parcourir 2 ou plsrs collections
+symboles = ['+','-','*','/']
+opérations=['addition','soustraction','multiplication','division']
+
+for objet in zip(symboles,opérations):
+    print(type(objet),objet)
+
+#autre syntaxe
+template = 'Le symbole de l\'opération {op} est {sym}'
+for symbole, opération in zip(symboles,opérations):
+    message = template.format(op=opération, sym=symbole)
+    print(message)
