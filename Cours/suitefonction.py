7ee12d3a893fbdbe1f5c3704cbbd031feeed11b5
Baovola1 <baovolarav@gmail.com>
2024-11-24 20:04:33 +0100

    cas possible de retour avec une fonction

--- Cours/suitefonction.py
+++ Cours/suitefonction.py
@@ -10,6 +10,36 @@ dire_bonjour_deux_fois()
 ##RETOURNER des valeurs avec les fonctions en Python
 def ma_fonction():
     print('Hey')
-    return 'VALEUR DE RETOUR'
+    return 'ON EST DANS LE RETURN'
 
 print(ma_fonction())
+
+#Plus d'explication sur le return
+def ma_fonction_avec():
+    print('Hey avec')
+    return 'AVEC VALEUR DE RETOUR'
+
+def ma_fonction_sans():
+    print('Hey sans')#Ici sans valeur retourné, on a un result 'None'
+
+avec = ma_fonction_avec()
+sans = ma_fonction_sans()
+
+print(avec)
+print(sans)
+
+#Les valeurs qu'on peut retourner:nbr,chaine de caractère,fonction,tuples, listes
+def fonction():
+    return 1
+
+print(fonction())
+
+def fonction1():
+    def function_interne():
+        print('Hello from the inside')
+    
+    return function_interne
+
+nom_externe = fonction1()
+
+nom_externe()
