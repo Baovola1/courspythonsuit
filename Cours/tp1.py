f27c61899e7c8d3edd519a37d810084c57944677
Baovola1 <baovolarav@gmail.com>
2024-11-24 11:10:00 +0100

    utilisation de la boucle for avec une liste pour obtenir le carré d'un nbr

--- Cours/tp1.py
+++ Cours/tp1.py
@@ -3,3 +3,10 @@ collection = ["A","B","C"]
 
 for alphabet in collection:
     print(alphabet.lower())
+
+#Exo2:Bien choisir ses noms de variables
+nombre_pairs=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+
+for nombre_pair in nombre_pairs:
+    carré = nombre_pair ** 2
+    print(carré)
