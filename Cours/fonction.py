286eced7cff66356379703d9cb6ec8faf87f275f
Baovola1 <baovolarav@gmail.com>
2024-11-24 18:46:07 +0100

    appel fonction dans un autre fichier

--- Cours/fonction.py
+++ Cours/fonction.py
@@ -1,5 +1,12 @@
-#Fonction
+
+
+#Règle de syntaxe sur la fonction en python
 def nom_de_variable():
     print('Hello world')
 
 nom_de_variable()
+
+#Ici, on importe la fonction qui est dans le fichier helper.py
+from helper import afficher_aide
+
+afficher_aide()
