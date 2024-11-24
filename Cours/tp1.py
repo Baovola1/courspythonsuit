7e33e0f2698ebd8006f6534cd9265a2dc68d253f
Baovola1 <baovolarav@gmail.com>
2024-11-24 11:26:19 +0100

    utilisation de la fonction enumerate avec la boucle for pour obtenir tous les nbr multiples de 3

--- Cours/tp1.py
+++ Cours/tp1.py
@@ -10,3 +10,18 @@ nombre_pairs=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
 for nombre_pair in nombre_pairs:
     carré = nombre_pair ** 2
     print(carré)
+
+#Exo3:Utilisation de la fonction built-in enumerate()plutôt que de créer des compteurs
+multiples_de_trois=list(range(0,100,3))
+
+
+for i,each in enumerate(multiples_de_trois):
+    # Ci-dessous, {i + 1} car i commence à zéro et on veut compter à partir de 1
+    print(f'Le multiple de trois n°{i+1} est {each}')
+
+#Autre solution de l'exo3 
+multiples_de_trois=list(range(0,100,3))
+
+
+for each in enumerate(multiples_de_trois):
+    print(each)
