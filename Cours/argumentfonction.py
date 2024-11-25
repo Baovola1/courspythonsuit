960096864aba5ea63e8cce532fad94823b9dd63d
Baovola1 <baovolarav@gmail.com>
2024-11-25 08:40:15 +0100

    Comment définir un argument par défaut dans une fonction

--- Cours/argumentfonction.py
+++ Cours/argumentfonction.py
@@ -1,4 +1,11 @@
+#Utilisation simple argument
 def dire_quelquechose(qqch):
     print(qqch)
 
 dire_quelquechose('Hey you')
+
+#Définir un argument par défaut
+def ma_fonction(mot='Message par défaut'):
+    print(mot)
+
+ma_fonction()
