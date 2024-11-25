c65cd06f0f3cd566253187c7ebfb22599bedab29
Baovola1 <baovolarav@gmail.com>
2024-11-25 08:49:08 +0100

    cas utilisation argument avec msg par défaut et un argument défini

--- Cours/argumentfonction.py
+++ Cours/argumentfonction.py
@@ -5,7 +5,13 @@ def dire_quelquechose(qqch):
 dire_quelquechose('Hey you')
 
 #Définir un argument par défaut
+#Cas1:il affiche le msg par défaut
 def ma_fonction(mot='Message par défaut'):
     print(mot)
 
 ma_fonction()
+#Cas2:le msg 'Hey you' s'affiche dans ce cas
+def ma_fonction1(msg='Message par défaut'):
+    print(msg)
+
+ma_fonction1('Hey You')
