7a751eabe5073cb491d8e5f3b57bde59126d9f6c
Baovola1 <baovolarav@gmail.com>
2024-11-30 22:39:34 +0100

    règle indexing des strings positifs et negatifs

--- Cours/string.py
+++ Cours/string.py
@@ -2,3 +2,6 @@
 #len()=> Savoir le nbr de caractère dans une chaine de caractère
 texte = 'hello'
 print(len(texte))
+#L'indexing des strings
+#index maximal: index du dernier element=>len(texte)-1
+#index négatif: -len(texte)=>limite a ne pas dépaC
